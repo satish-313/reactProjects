e9fbfb92253c6322792a4175bb1effd3ff9444df
satish-313 <pradhansatish53@gmail.com>
2020-12-08 20:14:00 +0530

    first project

---
+++ birthDayApp/src/data.js
@@ -0,0 +1,19 @@
+import image from './image/img.png';
+
+export default [
+  {
+    id:1,name:'satish kumar',age:22,image:image,
+  },
+  {
+    id:2,name:'satish kumar',age:22,image:image,
+  },
+  {
+    id:3,name:'satish kumar',age:22,image:image,
+  },
+  {
+    id:4,name:'satish kumar',age:22,image:image,
+  },
+  {
+    id:5,name:'satish kumar',age:22,image:image,
+  },
+]
