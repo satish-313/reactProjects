db7ba41c40d840cc17c5095fb581eb3ca1c9e566
satish-313 <pradhansatish53@gmail.com>
2020-12-13 19:12:21 +0530

    added 8-lorem-app

--- 8-lorem-app/src/App.js
+++ 8-lorem-app/src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+import data from './data';
 
 const App = () => {
+  const [count,setCount] = useState(0)
+  const [text,setText] = useState([]);
+
+  const number_of_paragraphs = (number) => {
+    if (number <= 0){
+      return 1
+    }
+    else if (number >= data.length){
+      return data.length - 1
+    }
+    else {
+      return number
+    }
+  }
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    let amout = parseInt(count)
+    setText(data.slice(0,number_of_paragraphs(amout)))
+  }
+
   return(
-    <div>
-      hello world
-    </div>
+    <section className="section-center">
+      <h3>Tired of boring lorem ipsum</h3>
+      <form className="lorem-form" onSubmit={handleSumbit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount" value={count} onChange={(e) => setCount(e.target.value)}/>
+        <button type="submit" className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        <hr/>
+        {text.map((p,index) => {
+          return (
+            <p key={index}>{p}</p>
+          )
+        })}
+      </article>
+    </section>
   )
 };
 
