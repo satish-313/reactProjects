42b09f836fda0fedbc9fffbb058099581f9ceb97
satish-313 <pradhansatish53@gmail.com>
2020-12-19 01:59:35 +0530

    added cocktail

---
+++ 15-cocktail-app/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return(
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo quam, dolore non eveniet nesciunt sapiente ea odit. Veniam doloribus laudantium assumenda neque corporis, excepturi, reprehenderit blanditiis recusandae amet vero facilis eligendi, id eveniet. Cumque aspernatur cupiditate, similique, iure omnis, nulla numquam reprehenderit aliquid in praesentium delectus magnam saepe ex aperiam!</p>
+    </section>
+  )
+}
+
+export default About;
